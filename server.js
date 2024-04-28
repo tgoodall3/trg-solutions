d909892a9dfa32956dd72bcbe2852b4003d7d40f
tgoodall3 <105135371+tgoodall3@users.noreply.github.com>
2024-04-28 12:08:16 -0400

    server changes

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/api/reviews', (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
